137f27fe7205eaba71219ffab135a37c63b1b1de
Fatima Zahra Hasbi <hasbi.fatimazahra04@gmail.com>
2022-12-23 11:52:24 +0100

    initial commit

---
+++ src/main/java/com/hasbi/framework_hasbi/FrameworkHasbiApplication.java
@@ -0,0 +1,13 @@
+package com.hasbi.framework_hasbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkHasbiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkHasbiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hasbi/framework_hasbi/FrameworkHasbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasbi.framework_hasbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkHasbiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
